bfa6e508009cee71bec5f7e35927ca1be4925697
amir <amirtaki623@gmail.com>
2025-04-26 19:39:28 +0330

    syntax props to props funtion in TS

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import User from "./component/User"
 
 
 
-
+function sum (a:number, b: number) : number{
+  return a + b;
+}
 function App() {
 
   return (
       <div className="App">
         <h1>type script</h1>
-        <User name = "amir" age = {28} hasChild = {false}  />
+        <User name = "amir" age = {28} hasChild = {false} sum = {sum}  />
       </div>
     );
   }
--- src/component/User.tsx
+++ src/component/User.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 
-type propsFunctions = {name : string , age : number, hasChild : boolean} 
+// نکته : وقتی قرار نیست چیزی برگرداند از 
+// void
+// استفاده میکنبم
 
-function User (props:propsFunctions) {
+type propsUser = {name : string ,
+    age : number, 
+    hasChild : boolean,
+    sum: (a : number , b:number)=> number 
+} 
+
+function User (props:propsUser) {
     return(
     <>
-        
         <h2> name :{props.name  }</h2>
+      
         <h2>age :{props.age}</h2>
+      
         <h2>hasChild : {`${props.hasChild}`}</h2>
+      
+        <h2>sum : {props.sum(2, 6)}</h2>
     </>    
     )
 }
