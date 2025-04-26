292c38b7655e72fbedf3f77e8882882c09c179a8
amir <amirtaki623@gmail.com>
2025-04-26 17:26:32 +0330

    type in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+
+  // type string
+  let name:string  = "typeScript";
+
+  // let test: string = 12; Error
+  
+  console.log(typeof(name));
+
+//   نکته : در تایپ اسکریپت نمیتوان تایپ یک متغییر تغییر داد. اما در جاوا اسکریپپ میتوان تغییر داد.
+  
+
+// type number
+
+let counter:number = 1;
+console.log(typeof(counter));
+
+
+// type boolean
+
+let flag:boolean = false;
+console.log(typeof(flag))
+
+
+//  نکته ما زمانی برای متغیر تایپ تعریف میکنیم همان نوعی که میخواهیم  از مقدار اولیه باشد.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>type script</h1>
     </div>
   );
 }
