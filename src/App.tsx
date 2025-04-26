a1cd8aa9628bee66b4853e9f8339fd7f3581463e
amir <amirtaki623@gmail.com>
2025-04-26 22:47:11 +0330

    useState type in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
-import Article from "./component/Article"
+import { useState } from 'react';
 
+
+
+type State = {
+  name : string,
+  age : number, 
+}
 
 function App() {
+
+  // const[state, setState] = useState<string>("test")
+
+  const[state, setState] = useState<State | null>(null);
 
   return (
       <div className="App">
         <h1>type script</h1>
-
-        <Article>
-           <h1>عنوان مقاله اول</h1>
-          سطر اول : شروع
-        </Article>
-
+          
+        {
+          state?.name
+        }
       </div>
     );
   }
 
 export default App;
+
+
+
+// هر نوع مقداری که در 
+// useState(مقدار)
+// بکذاریم همان تایپ آن میشود
+
+// --------------------------------------------
+
+// برای محدود کردن کافیست از دستور زیر استفاده کنیم
+
+// const [state, setState] = usestate<boolean>(false)
+
+// ------------------------------------
+// برای آبجکت چون نمیدانیم که چه نوع تایپی از بک اند میاد از 
+// null
+// استفاده میکنیم
+
+
+// const[state, setState] = useState<State | null>(null);
+
+// نکته : علت علامت سوال این است که قبل از رسیدن دیتا از بک اند مقدار
+// null
+// است
+
