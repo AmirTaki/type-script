64d2ce53393aa91cc59f72438abe60f83c1cdee6
amir <amirtaki623@gmail.com>
2025-04-26 23:20:18 +0330

    interface type in TypeScript

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 
-type TUser = {
-    value : string, 
-    onChange : React.Dispatch<React.SetStateAction<string>>; 
+// type TUser = {
+//     value : string, 
+//     onChange : React.Dispatch<React.SetStateAction<string>>; 
+// }
+
+
+interface IUser {
+    value : string,
+    onChange : React.Dispatch<React.SetStateAction<string>>
 }
 
-function User ({value, onChange}:TUser){ 
+type TFruits = "Banan" | "Apple" | "Orange" // Union Type
+
+interface INames { name : string}
+
+type TCars = {name : string}
+
+
+function User ({value, onChange}:IUser){ 
     return (
         <div>
             {value}
@@ -14,3 +27,15 @@ function User ({value, onChange}:TUser){
 }
 
 export default User;
+
+
+
+// برای تعریف تایپ برای آبجکت ها از اینترفیس استفاده میکنیم
+
+// کاربرد : اینترفیس ها معمولا برای آبجکت ها استفاده میشود و اما تایپ ها برای 
+
+// تایپ ها قابلیت بیشتری دارند معمولا برای مقدار دهی به متغییر ها استفاده میشود
+
+// مثلا در تایپ از 
+// union
+// میتوان استفاده کرد
