f9befe699486b94e6de0e8ad6d9d65d47fa1912a
amir <amirtaki623@gmail.com>
2025-04-26 17:53:06 +0330

    type in list & obj typeSctrpt

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,40 @@ console.log(typeof(flag))
 
 
 //  نکته ما زمانی برای متغیر تایپ تعریف میکنیم همان نوعی که میخواهیم  از مقدار اولیه باشد.
-  return (
+
+
+// تایپ آرایه ها :
+
+let array = [1,"string", false, ];
+
+console.log(typeof(array))
+
+let array2: number[] = [1, 2, 3, 4, 5]
+
+let array3: string[] = ["code", "typeScript"]
+
+let array4: (string  | number | boolean) [] = [true, 1, 2, 3, 4, "typeScript"]
+
+
+// آبجکت ها
+
+let obj_user = {
+  name : "amir",
+  age : 28,
+  color : "white",
+}
+
+// تعریف تایپ برای آبجکت ها
+
+let obj_user2 : {name : string ; age : number ; color : string} = {
+  name : "amir",
+  age : 28,
+  color : "white"
+}
+
+
+
+return (
     <div className="App">
       <h1>type script</h1>
     </div>
