16671aebbfdd43b103c7119fc21c909336e4ca08
amir <amirtaki623@gmail.com>
2025-04-26 22:58:34 +0330

    type useSate  propes in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,24 @@
 import React from 'react';
 import { useState } from 'react';
+import User from './component/User';
 
-
-
-type State = {
-  name : string,
-  age : number, 
-}
 
 function App() {
 
-  // const[state, setState] = useState<string>("test")
+  const[state, setState] = useState<string>("amir")
 
-  const[state, setState] = useState<State | null>(null);
+  // const[state, setState] = useState<string|null>(null)
+
 
   return (
       <div className="App">
         <h1>type script</h1>
           
-        {
-          state?.name
-        }
+          <User value = {state} onChange = {setState} />
+       
       </div>
     );
   }
 
 export default App;
-
-
-
-// هر نوع مقداری که در 
-// useState(مقدار)
-// بکذاریم همان تایپ آن میشود
-
-// --------------------------------------------
-
-// برای محدود کردن کافیست از دستور زیر استفاده کنیم
-
-// const [state, setState] = usestate<boolean>(false)
-
-// ------------------------------------
-// برای آبجکت چون نمیدانیم که چه نوع تایپی از بک اند میاد از 
-// null
-// استفاده میکنیم
-
-
-// const[state, setState] = useState<State | null>(null);
-
-// نکته : علت علامت سوال این است که قبل از رسیدن دیتا از بک اند مقدار
-// null
-// است
 
--- src/component/User.tsx
+++ src/component/User.tsx
@@ -1,38 +1,15 @@
 import React from "react";
 
-// میتوانیم بعضی تایپ ها را به صورت خاص تعریف کنیم
+type TUser = {
+    value : string, 
+    onChange : React.Dispatch<React.SetStateAction<string>>; 
+}
 
-type UserMoter = "mari" | "didi" | "jeni" ;
-
-type USER = {
-    name : string ,
-    
-    age : number, 
-    
-    hasChild : boolean,
-    
-    father? : string,
-
-    mother : UserMoter,
-
-    sum: (a : number , b:number)=> number 
-} 
-
-function User ({name, age , hasChild, sum, father, mother}:USER) {
-    return(
-    <>
-        <h2> name :{name}</h2>
-      
-        <h2>age :{age}</h2>
-      
-        <h2>hasChild : {`${hasChild}`}</h2>
-      
-        <h2>sum : {sum(2, 6)}</h2>
-      
-        <h2> father : {father ?  `${father}` :  "ندارد" } </h2>   
-
-        <h2>motehr : {`${mother}`}</h2>      
-    </>    
+function User ({value, onChange}:TUser){ 
+    return (
+        <div>
+            {value}
+        </div>
     )
 }
 
