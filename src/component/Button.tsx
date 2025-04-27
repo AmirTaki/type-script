4f7f8ccb56b97c6a4d7d9e60e31ea86015545300
amir <amirtaki623@gmail.com>
2025-04-27 18:41:38 +0330

    component propes part 2 in TypeScript

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -1,16 +1,12 @@
-import React, { ComponentProps } from "react";
+import React, { ComponentProps, ComponentPropsWithoutRef, ComponentPropsWithRef } from "react";
  
-// type TButton = {
-//     type? : string, 
-//     value? : string ,
-//     onClick? : () => void;
-// }
 
-type TButton = ComponentProps<"button"> & {
+
+type TButton = ComponentPropsWithRef <"button"> & {
     buttonTitle? : string
 }
 
-function Button ({ type, onChange, onClick, buttonTitle} : TButton) {
+function Button ({ type, onChange, onClick, buttonTitle, ref} : TButton) {
     return(
         <>
             <button type = {type}>
@@ -23,6 +19,19 @@ function Button ({ type, onChange, onClick, buttonTitle} : TButton) {
 export default Button; 
 
 
-// نکته کاهی میخواهیم تایپ اضافه بگداریم از 
-// & 
-// استفاده میکنیم
+
+// اکر  میخواهیم از
+// ref
+// استفاده کنیم و 
+// ref
+// پاس دهیم به کامپوننت از دستور زیر استفاده میکنیم
+
+// ComponentPropsWithRef
+
+// ComponentProps
+
+// و اما اکر نخواهیم از 
+// ref 
+// استفاده نکنیم از دستور زیر استفاده میکنیم
+
+// ComponentPropsWithoutRef
