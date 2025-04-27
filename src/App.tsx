31e16081e680833044820be4ef1ace6d3fa09e2e
amir <amirtaki623@gmail.com>
2025-04-27 19:27:51 +0330

    toturial TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,42 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 
-type TUser =  string; 
+
 
 function App() {
-
-  useEffect (()=>{
-
-    // const user = localStorage.getItem("user")
-
-    const user = JSON.parse(localStorage.getItem("user") as TUser );
-
-    console.log(typeof user)
-
-    console.log(user)
-  
-  })
-
 
 
   return (
       <div className="App">
         <h1>type script</h1>
-
+        <h3>Toturial TypeScript </h3>
+        <p>
+        550961e as Type in TypeScript <hr></hr>
+        076feff Omit Type in TypeScript<hr></hr>
+        5860fd1 userRef Type in TypeScript<hr></hr>
+        3c0cba4 event Type in TypeScript<hr></hr>
+        4f7f8cc component propes part 2 in TypeScript<hr></hr>
+        8781800 component propes part 1 in TypeScript<hr></hr>
+        61eccc1 extand type in TypeScript<hr></hr>
+        64d2ce5 interface type in TypeScript<hr></hr>
+        16671ae type useSate  propes in TypeScript<hr></hr>
+        a1cd8aa useState type in TypeScript<hr></hr>
+        a27e952 type children  in TypeScript<hr></hr>
+        ee0af6a union type in TypeScript<hr></hr>
+        a54de69 type any in typeScript<hr></hr>
+        f706d9f type optional in typeScript<hr></hr>
+        eb38c6b react fc in typeScript<hr></hr>
+        bfa6e50 syntax props to props funtion in TS<hr></hr>
+        ee5ee1f syntax props Functions in typeScript<hr></hr>
+        6a74aa8 syntax funtion in typeScript<hr></hr>
+        060af1f syntax typeScript 1<hr></hr>
+        f9befe6 type in list & obj typeSctrpt<hr></hr>
+        292c38b type in ts<hr></hr>
+        74029aa Initialize project using Create React App<hr></hr>
+        </p>
       </div>
     );
   }
 
 export default App;
 
-
-// نکته هر جا که متغییر را ما دو حالتی باشد یا 
-
-// null
-
-// باشد یا
-
-// undifind
-
-// باشد میتوان از 
-
-// as 
-
-// استفاده کرد
-
-
-// یا بخواهیم استرینک به عنوان یه نامبر جا بزنیم یا بالعکس
-
-// یا از 
-
-// parseInt
-
-// toString
