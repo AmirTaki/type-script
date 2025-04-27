076feff72e5156b5abf4b097ba763f961ea75f35
amir <amirtaki623@gmail.com>
2025-04-27 19:03:37 +0330

    Omit Type in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React, { useRef } from 'react';
 
+type User1 = {
+  name : string,
+  age : number,
+  color : string
+}
+
+type user2 = Omit<User1, "age" | "color">
+  
 
 
 function App() {
 
-  
-  const ref = useRef<HTMLButtonElement>(null)
-
-  const inputRef = useRef<HTMLInputElement>(null)
 
   return (
       <div className="App">
         <h1>type script</h1>
-
-        <button ref = {ref}> Click </button> 
-
-        <input type="text" ref = {inputRef} />
 
       </div>
     );
